9cfcaf8965c8ed8e5616644875edef2f434609ce
adeelbaig625 <adeelbaig625@gmail.com>
2022-11-27 16:28:12 +0500

    jwt-strategy

---
+++ Model/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please add an email"],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please add a valid email",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+    minlength: 6,
+    select: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model("User", UserSchema);
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./connectDb");
+const passport = require("passport");
 const port = 3000;
-
+app.use(express.json());
+app.use(passport.initialize());
+require("./config/passport")(passport);
 app.get("/", (req, res) => {});
 
 app.listen(port, async () => {
